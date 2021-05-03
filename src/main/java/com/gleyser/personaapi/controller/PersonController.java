5974ebe0dc3a7afc7a8b217b01f2840fddf9d28e
Gleyser Guimarães <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2021-05-03 05:15:22 -0300

    add readme

--- src/main/java/com/gleyser/personaapi/controller/PersonController.java
+++ src/main/java/com/gleyser/personaapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.gleyser.personaapi.dto.request.PersonDTO;
 import com.gleyser.personaapi.dto.response.MessageResponseDTO;
 import com.gleyser.personaapi.exception.PersonNotFoundException;
 import com.gleyser.personaapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private final PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService){
-        this.personService =personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
